ca3695e5b6f6c556c97f1d2b703691e024b66a0f
Faduma <faduma@Fadumas-MacBook-Air.local>
2022-11-24 20:36:58 +0000

    first commit

---
+++ ReadME.md
@@ -0,0 +1,18 @@
+# 03 JavaScript: Password Generator
+
+## Project Description:
+This project allows people to be able to generate a very secure will protect password wiht just one click. It can be customised to having as many charcters as you would want and different special characters. 
+
+
+## How to Install and Run the Project:
+This project can be cloned from my repository via the git hub website
+by following the link
+
+and can be cloned to your personal repo.
+
+## How to Use the Project:
+Click generate password and apply all the criteria you want included in your password, in a second you will see a new passord.
+
+
+## Website image:
+![screenshot-of-applicaton](/image/_Users_faduma_Documents_Uob-Challenage-Repo_Week%25203_index.html%20(1).png)
---
+++ script.js
@@ -0,0 +1,72 @@
+var characterlength = 8;
+var choiceArr = [];
+
+var specialCharArr = ['!','@','#','$','%','^','&','*','(',')','[',']','{','}','|','?','>','<','|']
+var lowerCassArr = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+var upperCassArr = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+var numberArr = ['1','2','3','4','5','6','7','8','9','0']
+
+
+
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+// Write password to the #password input
+function writePassword() {
+    var correctPrompts = generatePrompts();
+    var passwordText = document.querySelector("#password");
+  
+    if (correctPrompts) {
+        var newpassword = generatePassword();
+        passwordText.value = newpassword;
+        }
+        else {
+        passwordText.value = "";
+        }
+
+}
+
+function generatePassword(){
+//generate password based on the promts
+ var password = ""
+ for( var i = 0; i < characterlength; i++) {
+    var randomIndex = Math.floor(Math.random() * choiceArr.length);
+    password = password + choiceArr[randomIndex];
+}
+return password; 
+}
+
+
+function generatePrompts() {
+    choiceArr = [];
+ characterlength = parseInt(prompt("How many characters would you like in your password?(8 - 128 character")); //NaN
+  if(isNaN(characterlength) || characterlength < 8 || characterlength > 128) { // i hope that this is all false 
+    alert("Character length has to be a number, 8 - 128 digits. Please try again.");
+    return false;
+  }
+
+  if (confirm("Would you like lowercase letters in your password")) {
+    choiceArr = choiceArr.concat(lowerCassArr);
+  }
+  if (confirm("Would you like uppercase letters in your password")) {
+    choiceArr = choiceArr.concat(upperCassArr);
+  }
+  if (confirm("Would you like speical characters in your password")) {
+    choiceArr = choiceArr.concat(specialCharArr);
+  }
+  if (confirm("Would you like numbers in your password")) {
+    choiceArr = choiceArr.concat(numberArr);
+  }
+  return true;
+
+
+}
+
+
+
+
+
+
